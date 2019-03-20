40e762e4a0075f91c9fa03ddf0c6e3dcdc080d8b
user1 <user1@LP-71VDTS1.HCLT.CORP.HCL.IN>
2019-03-20 16:41:08 +0530

    formatting

--- src/main/java/com/ing/Controller/ProductGroupController.java
+++ src/main/java/com/ing/Controller/ProductGroupController.java
@@ -13,30 +13,20 @@ import com.ing.domain.ProductGroup;
 import com.ing.service.ProductGroupService;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @CrossOrigin
 @RestController
 @EnableSwagger2
 @RequestMapping("api/v1/")
 public class ProductGroupController {
-	
+
 	@Autowired
 	ProductGroupService productGroupService;
-	/*@PostMapping("spendingCategory")
-	public ResponseModel CreateSpendingCategory(@RequestParam String name) {
-		
-		ResponseModel responseModel = new ResponseModel();
-		responseModel.setStatus(true);
-		responseModel.setMessage("sucess");
-		SpendingCategoryService.CreateSpendingCategory(name);
-		
-		return responseModel;
-	} */
-	
 
 	@GetMapping("/groups")
 	public ResponseEntity<List<ProductGroup>> getGroups() {
-	List<ProductGroup>	 list = productGroupService.getAllProductGroups();
-		return  ResponseEntity.ok().body(list);
-	} 
+		List<ProductGroup> list = productGroupService.getAllProductGroups();
+		return ResponseEntity.ok().body(list);
+	}
 
 }
