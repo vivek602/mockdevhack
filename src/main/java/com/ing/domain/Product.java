5e09f2977f5c2c363e957bf581728fa7f51243d2
vivek602 <sanu.singh60223@gmail.com>
2019-03-20 19:02:50 +0530

    Checkins for modified table attributes for Saving Product.

--- src/main/java/com/ing/domain/Product.java
+++ src/main/java/com/ing/domain/Product.java
@@ -13,9 +13,16 @@ public class Product {
 	@GeneratedValue
 	private int id;
 	private String name;
-	private String product_type;
-	private Long balance;
-	private Long max_limit;
+	private String interest_rate;
+	private double percentage;
+	private String special;
+	private String putinandwithdrawel;
+	private double mininvestment;
+	private double maxinvestment;
+	private String duration;
+	//private String product_type;
+	//private Long balance;
+	//private Long max_limit;
 
 	public int getId() {
 		return id;
@@ -33,28 +40,75 @@ public class Product {
 		this.name = name;
 	}
 
-	public String getProduct_type() {
-		return product_type;
+	/*
+	 * public String getProduct_type() { return product_type; }
+	 * 
+	 * public void setProduct_type(String product_type) { this.product_type =
+	 * product_type; }
+	 * 
+	 * public Long getBalance() { return balance; }
+	 * 
+	 * public void setBalance(Long balance) { this.balance = balance; }
+	 * 
+	 * public Long getMax_limit() { return max_limit; }
+	 * 
+	 * public void setMax_limit(Long max_limit) { this.max_limit = max_limit; }
+	 */
+
+	public String getInterest_rate() {
+		return interest_rate;
 	}
 
-	public void setProduct_type(String product_type) {
-		this.product_type = product_type;
+	public void setInterest_rate(String interest_rate) {
+		this.interest_rate = interest_rate;
 	}
 
-	public Long getBalance() {
-		return balance;
+	public double getPercentage() {
+		return percentage;
 	}
 
-	public void setBalance(Long balance) {
-		this.balance = balance;
+	public void setPercentage(double percentage) {
+		this.percentage = percentage;
 	}
 
-	public Long getMax_limit() {
-		return max_limit;
+	public String getSpecial() {
+		return special;
 	}
 
-	public void setMax_limit(Long max_limit) {
-		this.max_limit = max_limit;
+	public void setSpecial(String special) {
+		this.special = special;
+	}
+
+	public String getPutinandwithdrawel() {
+		return putinandwithdrawel;
+	}
+
+	public void setPutinandwithdrawel(String putinandwithdrawel) {
+		this.putinandwithdrawel = putinandwithdrawel;
+	}
+
+	public double getMininvestment() {
+		return mininvestment;
+	}
+
+	public void setMininvestment(double mininvestment) {
+		this.mininvestment = mininvestment;
+	}
+
+	public double getMaxinvestment() {
+		return maxinvestment;
+	}
+
+	public void setMaxinvestment(double maxinvestment) {
+		this.maxinvestment = maxinvestment;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
 	}
 
 	public Product(int id) {
