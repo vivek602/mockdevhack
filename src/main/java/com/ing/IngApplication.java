62d7bd3a2d7587f707de63d8067125d49404ec9b
user1 <user1@LP-BL8G8R1.HCLT.CORP.HCL.IN>
2019-03-20 12:52:24 +0530

    base structure

---
+++ src/main/java/com/ing/IngApplication.java
@@ -0,0 +1,13 @@
+package com.ing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/IngApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IngApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
